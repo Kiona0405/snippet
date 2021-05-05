5c1c9c9c9117547b1fdaa18276cb160e06a4e674
Kiona0405 <selfhelp.nt@gmail.com>
2021-05-05 17:44:41 +0900

    add snippet

---
+++ cpp/header.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define rep(i,n,m) for(int i = (n); i <(m); ++i)
+#define rrep(i,n,m) for(int i = (n-1); i >=(m); --i)
+
+using namespace std;                                            
+using ll = long long;
+
+int main() {
+    return 0;
+}
